57b08dd336240188a8e6c8d12e1832201eceebf9
lizbeth-r <aglae.ramirez@sdgku.edu>
2025-01-14 17:55:39 -0800

    Updated version

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import login
+from .views import UserLoginView, user_logout, SignUpView
 
 urlpatterns = [
-    path('login/', login, name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import redirect
+from django.contrib.auth import logout
+from django.views.generic.edit import CreateView
+from .forms import SignupForm
 
+"""
+Class-based views:
+
+View = generic view
+ListView = get a list of records
+DetailView = get a single(detail) record
+CreateView = create a new record
+DeleteView = remove a record
+UpdateView = modify an existing record
+LoginView  = login
+"""
 # Create your views here.
-def login(request):
-    return render(request, 'users/login.html')
+class UserLoginView(LoginView):
+    template_name = "users/login.html"
+    
+    def get_success_url(self):
+        return reverse('home')
+
+class SignUpView(CreateView):
+    template_name = "users/signup.html"
+    form_class = SignupForm
+    success_url = reverse_lazy('login')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
 
 
